85b279589b61ddaf9da541e2236c8fa408e1f084
daniel <maherydn@gmail.com>
2025-03-10 09:56:58 +0300

    changement de bg, ajout image

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
 const menuToggle = document.getElementById("menu-toggle");
-      const mobileMenu = document.getElementById("mobile-menu");
+const mobileMenu = document.getElementById("mobile-menu");
 
-      menuToggle.addEventListener("click", () => {
-        mobileMenu.classList.toggle("hidden");
-      });
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
 
-      window.addEventListener("scroll", () => {
-        const header = document.getElementById("header");
-        const scroll = window.scrollY;
+window.addEventListener("scroll", () => {
+  const header = document.getElementById("header");
+  const scroll = window.scrollY;
 
-        if (scroll > 0) {
-          header.classList.remove("h-24");
-          header.classList.add("h-14");
-        } else {
-          header.classList.remove("h-14");
-          header.classList.add("h-24");
-        }
-      });
+  if (scroll > 0) {
+    header.classList.remove("h-24");
+    header.classList.add("h-14");
+  } else {
+    header.classList.remove("h-14");
+    header.classList.add("h-24");
+  }
+});
 
-      
-      function downloadCV() {
-        const link = document.createElement("a");
-        link.href = "./CV.pdf";
-        link.download = "CV.pdf";
-        link.click();
-      }
+function downloadCV() {
+  const link = document.createElement("a");
+  link.href = "./CV.pdf";
+  link.download = "CV.pdf";
+  link.click();
+}
